118914e0edc676bd6e8daa529c79496b2d3095c2
Pavlo Bystrytskyi <pavlo.bystrytskyi@gmail.com>
2024-10-23 15:34:34 +0200

    ISSUE-40: Implement the EditMovieForm Component.

--- frontend/src/Component/App/Main/Tab/MovieList/ListElement.tsx
+++ frontend/src/Component/App/Main/Tab/MovieList/ListElement.tsx
@@ -1,17 +1,20 @@
 import MovieRatingType from "../../../../../Type/MovieRatingType.tsx";
+import {useNavigate} from "react-router-dom";
 
 export default function ListElement({movie}: { movie: MovieRatingType }) {
+    const navigate = useNavigate();
+
+    const redirect = function () {
+        navigate("/movie/" + movie.movieId);
+    }
+
     return (
         <>
             <div className={"list-element"}>
                 <span className={"movie-name"}>{movie.movieName}</span>
-                <span className="movie-rating">
-  {"★".repeat(movie.rating)}
-                    <span className="empty-stars">
-    {"★".repeat(10 - movie.rating)}
-  </span>
-</span>
-                <button className={"edit-button"}>Edit</button>
+                <span className="movie-rating">{"★".repeat(movie.rating)}</span>
+                <span className="empty-stars">{"★".repeat(10 - movie.rating)}</span>
+                <button className={"edit-button"} onClick={redirect}>Details</button>
             </div>
         </>
     );
