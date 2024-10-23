04eeeab41755d94487e6e0b1d5a1e2fa30a8fa3d
kristijanjakimov <kiko.jakimov@gmail.com>
2024-10-23 18:07:26 +0200

    Creating function created

--- frontend/src/Component/App/Main.tsx
+++ frontend/src/Component/App/Main.tsx
@@ -2,11 +2,34 @@ import WatchedTab from "./Main/WatchedTab.tsx";
 import WishlistTab from "./Main/WishlistTab.tsx";
 import EditMovieForm from "./EditMovieForm.tsx";
 import CreateButton from "./Main/CreateButton.tsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import MovieType from "../../Type/MovieType.tsx";
 import RatingType from "../../Type/RatingType.tsx";
+import axios from "axios";
+import MovieRatingType from "../../Type/MovieRatingType.tsx";
 
 export default function Main() {
+    const [wishListData, setWishListData] = useState<MovieRatingType[]>([]);
+    const [watchedListData, setwatchedListData] = useState<MovieRatingType[]>([]);
+
+    const [isUpdated, setIsUpdated] = useState(false);
+    useEffect(() => {
+        axios.get<MovieRatingType[]>("/api/movie/wishlist").then(
+            (result) => {
+                setWishListData(result.data)
+            }
+        ).catch(
+            () => console.log("Something went wrong")
+        )
+        axios.get<MovieRatingType[]>("/api/movie/watched").then(
+            (result) => {
+                setwatchedListData(result.data)
+            }
+        ).catch(
+            () => console.log("Something went wrong")
+        )
+        setIsUpdated(false);
+    }, [isUpdated]);
 
     const [movieData, setMovieData] = useState<MovieType>({
         id: "",
@@ -22,11 +45,12 @@ export default function Main() {
     return (
         <>
             <div className={"main_container"}>
-                <WatchedTab/>
-                <WishlistTab/>
+                <WatchedTab data={watchedListData}/>
+                <WishlistTab data={wishListData}/>
             </div>
-            <EditMovieForm id={undefined} movieData={movieData} ratingData={ratingData} setMovieData={setMovieData} setRatingData={setRatingData}/>
-            <CreateButton movieData={movieData} ratingData={ratingData}/>
+            <EditMovieForm id={undefined} movieData={movieData} ratingData={ratingData} setMovieData={setMovieData}
+                           setRatingData={setRatingData}/>
+            <CreateButton movieData={movieData} ratingData={ratingData} setIsUpdated={setIsUpdated}/>
         </>
     );
 }
--- frontend/src/Component/App/Main/CreateButton.tsx
+++ frontend/src/Component/App/Main/CreateButton.tsx
@@ -3,9 +3,10 @@ import RatingType from "../../../Type/RatingType.tsx";
 import MovieType from "../../../Type/MovieType.tsx";
 
 export default function CreateButton(
-    {ratingData, movieData}:
+    {ratingData, movieData, setIsUpdated}:
         {
             ratingData: RatingType, movieData: MovieType
+            setIsUpdated: (state: boolean) => void
         }) {
 
     const create = async function () {
@@ -13,11 +14,13 @@ export default function CreateButton(
             const response = await axios.post<MovieType>(`/api/movie`, movieData);
             ratingData.movieId = response.data.id;
             await axios.post(`/api/rating`, ratingData);
+            setIsUpdated(true);
         } catch {
             alert("Something went wrong");
         } finally {
             // TODO
         }
+
     }
 
     return (
