29bd3a47f2913e4ea6a46b2395b589161a7accca
kristijanjakimov <kiko.jakimov@gmail.com>
2024-10-25 10:20:05 +0200

    Fields are reseted after creation of a movie

--- frontend/src/Component/App/Header.tsx
+++ frontend/src/Component/App/Header.tsx
@@ -1,8 +1,10 @@
 import axios from "axios";
 import {useEffect, useState} from "react";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 export default function Header() {
+    const navigate = useNavigate();
+
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [userName, setUserName] = useState('');
 
@@ -22,34 +24,35 @@ export default function Header() {
                 setIsLoggedIn(false);
             })
     }
+
     const logout = () => {
-        const host = window.location.host === 'localhost:5173' ? 'http://localhost:8080' : window.location.origin
-        window.open(host + "/api/auth/logout", '_self');
-    };
-    const handleButtonClick = () => {
-        if (isLoggedIn) {
-            logout();
-        } else {
-            login();
-        }
-    };
+        axios.post(`/api/auth/logout`, {}).then(() => {
+            navigate("/")
+        })};
+        const handleButtonClick = () => {
+            if (isLoggedIn) {
+                logout();
+            } else {
+                login();
+            }
+        };
 
-    useEffect(() => {
-        loadUser()
-    }, []);
+        useEffect(() => {
+            loadUser()
+        }, []);
 
-    return (
-        <nav>
-            <Link to="/" className="navbar-title">
-                <i className="fa-duotone fa-solid fa-video"></i>
-                <h1>Movie Library</h1>
-            </Link>
-            <div className={"navbar-user"}>
-                <p>Hello {userName}</p>
-                <button onClick={handleButtonClick}>
-                    {isLoggedIn ? 'Logout' : 'Login'}
-                </button>
-            </div>
-        </nav>
-    );
-}
+        return (
+            <nav>
+                <Link to="/" className="navbar-title">
+                    <i className="fa-duotone fa-solid fa-video"></i>
+                    <h1>Movie Library</h1>
+                </Link>
+                <div className={"navbar-user"}>
+                    <p>Hello {userName}</p>
+                    <button onClick={handleButtonClick}>
+                        {isLoggedIn ? 'Logout' : 'Login'}
+                    </button>
+                </div>
+            </nav>
+        );
+    }
